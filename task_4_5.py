7d1a38eb2974033d97a57e3ba0c48b56bfbab99b
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2023-05-03 21:55:04 +0300

    finished

--- task_4_5.py
+++ task_4_5.py
@@ -2,5 +2,7 @@
 
 # SELECT * FROM customers WHERE city = 'London';
 
+
 # 5)Write a query in postgresql to retrieve the total number of orders placed by a customer with a given "customer_id" value '1234' from a table named "orders".
 # SELECT COUNT(*) FROM orders WHERE customer_id = '1234';
+
