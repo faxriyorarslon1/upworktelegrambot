50e96b8f74d472d6b99a3eb2161c9acf314f773c
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2023-05-03 22:58:05 +0300

    change task1 to aiogram 3

--- task1.py
+++ task1.py
@@ -1,20 +1,35 @@
 # 1. Create a bot that will reply the user's first name for any messages sent to the bot.
+import asyncio
 import logging
-import aiogram
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
 
-token = 'token'
-logging.basicConfig(level=logging.INFO)
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message
 
-bot = Bot(token=token)
-dp = Dispatcher(bot)
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "6231010696:AAHdx578ciB1_1OWILs2lFUhWP2hyQ4GQbs"
+
+# All handlers should be attached to the Router (or Dispatcher)
+router = Router()
 
 
-@dp.message_handler()
+@router.message()
 async def handle_message(message: types.Message):
-    await message.reply(f"Hello, {message.from_user.first_name}!")
+    await message.answer(f"Hello, <b>{message.from_user.full_name}!</b>")
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+async def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode="HTML")
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
